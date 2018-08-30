6c2080069841e0025af08aa8c04abbde9529e35b
a27 <andy27ds@mac.com>
2018-08-30 15:11:24 +0200

    First commit, late but still an commit

---
+++ Assets/Scripts/AudioSourceController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceController : MonoBehaviour {
+
+
+    public AudioClip blip;
+    public AudioClip dead;
+    public AudioClip end;
+    AudioSource audioSource;
+
+
+    private void OnEnable()
+    {
+        ParachuterController.Move += PlayBlipAudio;
+        ParachuterController.Inwater += PlayDeadAudio;
+    }
+
+    private void OnDisable()
+    {
+        ParachuterController.Move -= PlayBlipAudio;
+        ParachuterController.Inwater -= PlayDeadAudio;
+    }
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    public void PlayDeadAudio()
+    {
+        audioSource.PlayOneShot(dead, 1.0f);
+    }
+
+    public void PlayEndAudio()
+    {
+        audioSource.PlayOneShot(end, 1.0f);
+    }
+
+    public void PlayBlipAudio()
+    {
+        audioSource.PlayOneShot(blip, 1.0f);
+    }
+	
+}
